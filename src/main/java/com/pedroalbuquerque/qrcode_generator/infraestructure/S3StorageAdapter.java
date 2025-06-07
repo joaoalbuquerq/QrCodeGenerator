c2937a679a75a55a9fae42524c95e76dd72fd294
João Albuquerque <joao.albuquerque@gruporas.com.br>
2025-06-07 19:15:32 -0300

    adicionando infecao do spring

--- src/main/java/com/pedroalbuquerque/qrcode_generator/infraestructure/S3StorageAdapter.java
+++ src/main/java/com/pedroalbuquerque/qrcode_generator/infraestructure/S3StorageAdapter.java
@@ -2,11 +2,13 @@ package com.pedroalbuquerque.qrcode_generator.infraestructure;
 
 import com.pedroalbuquerque.qrcode_generator.ports.StoragePort;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
+@Component
 public class S3StorageAdapter implements StoragePort {
 
     private final S3Client s3Client;
